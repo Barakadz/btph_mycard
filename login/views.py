41acf2800ac2d4155cd1feefde5d0ae72c5acd80
Barakadz <121547694+Barakadz@users.noreply.github.com>
2024-04-16 09:45:12 +0200

    Update views.py

--- login/views.py
+++ login/views.py
@@ -1,18 +1,11 @@
 from flask import Flask,render_template, Blueprint, jsonify, request,redirect,url_for,make_response
-from flaskext.mysql import MySQL
 
 
 
 login=Blueprint('login',__name__,template_folder='templates',static_folder='static',static_url_path='/login/static')
 
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'btph'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost' # or your MySQL host
 
-mysql = MySQL()
-mysql.init_app(app)
  
  
 
@@ -23,22 +16,7 @@ def index():
         return response
      
     else:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS btph")
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS mycard (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            image VARCHAR(255) NOT NULL,
-            fonction VARCHAR(255) NOT NULL,
-            nom_entreprise VARCHAR(255) NOT NULL,
-            groupe_s VARCHAR(255) NOT NULL          
-            )
-        ''') 
-        conn.commit()
-        cursor.close()
-        conn.close()
+       
         return render_template('index.html',error='')
 
 @login.route('/login', methods=['POST','GET'])
